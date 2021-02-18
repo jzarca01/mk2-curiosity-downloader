6e61674fe050b0ceecab00fad020005c5d86b5ad
Jérémie Zarca <jeremie.zarca@gmail.com>
2021-02-18 17:38:49 +0100

    added cli list selector

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Mk2 = require("./lib/mk2");
 const Inplayer = require("./lib/inplayer");
 const Vimeo = require("./lib/vimeo");
 
+const inquirer = require("inquirer");
 const ora = require("ora");
 
 const spinner = ora(``);
@@ -25,10 +26,29 @@ const restartSpinner = (newText, isSuccessful = true) => {
     restartSpinner("Fetching free movies of the week");
     const episode = await Mk2.getEpisodeOfTheWeek();
     const films = await Mk2.getFilmsByEpisode(episode.uid);
+    spinner.stop();
+    console.log(`\nThere are ${films.length} movies available for download\n`);
 
-    console.log(`\nThere are ${films.length} movies available for download\n`)
+    const moviesToDL = await inquirer.prompt([
+      {
+        type: "checkbox",
+        message: "Which movies to download ?",
+        name: "choices",
+        choices: films.map((film) => ({
+          name: film.data.title,
+          value: film,
+          checked: true,
+        })),
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return "You must choose at least one movie.";
+          }
+          return true;
+        },
+      },
+    ]);
 
-    for (const film of films) {
+    for (const film of moviesToDL.choices) {
       restartSpinner(`Fetching URL for ${film.data.title}`);
       const links = await Inplayer.getUrlFromVideo(film.data.video_id);
       restartSpinner(`Downloading ${film.data.title}`);
